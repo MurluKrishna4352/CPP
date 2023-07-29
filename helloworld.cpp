0f347b90f094f78f5b43d7a07ebf524423e626bf
Krishna Nagpal <python.gamesaker@gmail.com>
2023-07-29 16:14:15 +0530

    adding hello world program

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define MODI 998244353
+#define INF le18
+#define PI 3.141592653589793238462
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using lld = long double;
+signed main(){
+    cout<<"Hello World!";
+}
