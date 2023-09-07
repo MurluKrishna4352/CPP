b366441556dfbf584649a15d61763db67fe83ced
Krishna Nagpal <python.gamesaker@gmail.com>
2023-09-07 16:11:45 +0400

    updating hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
 signed main(){
-    cout<<"Hello World!";
+    cout<<"Hello World!"<<endl;
+    cout<<"adding new line"<<endl;
+    cout<<"editing in vs code ☺";
 }
