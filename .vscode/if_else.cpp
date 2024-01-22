446a3c744f2ec3b37e73a440f2c98fef5832e6df
Krishna Nagpal <python.gamesaker@gmail.com>
2024-01-22 21:29:59 +0530

    adding if else

---
+++ .vscode/if_else.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define MODI 998244353
+#define INF le18
+#define PI 3.141592653589793238462
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using lld = long double;
+signed main(){
+    int num;
+    cin>>num;
+    if (num>0){
+        cout<<"+ve";
+    }
+    else if(num<0){
+        cout<<"-ve";
+    }
+    else{
+        cout<<"number is zero";
+    }
+}
