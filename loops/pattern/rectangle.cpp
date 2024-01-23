dc597f9612b79bc32cbbbb8fb0b272227996d958
Krishna Nagpal <python.gamesaker@gmail.com>
2024-01-23 13:06:56 +0530

    rectangle pattern

---
+++ loops/pattern/rectangle.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define MODI 998244353
+#define INF le18
+#define PI 3.141592653589793238462
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using lld = long double;
+signed main(){
+    for(int i=0; i<3; i++){
+        for(int j =0;j<5; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+
+    }
+}
